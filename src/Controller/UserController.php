bebb15ed5c7c663d4fc166b2ead161744ab63203
Ludo Drapo <ludodrapo@gmail.com>
2021-09-02 12:27:25 +0200

    Create and delete users routes added - validator installed - first constraints added

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,22 +4,27 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
-use FOS\RestBundle\Controller\Annotations\Get;
-use Symfony\Component\HttpFoundation\Response;
-use FOS\RestBundle\Controller\Annotations\View;
-use FOS\RestBundle\Controller\Annotations\Route;
+use App\Repository\ClientRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use JMS\Serializer\SerializerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * Class UserController
  * @package App\Controller
- * @Route("/users")
+ * @Rest\Route("/users")
  */
 class UserController extends AbstractFOSRestController
 {
     /**
-     * @Get(name="api_users_list")
-     * @View
+     * @Rest\Get(name="api_users_list")
+     * @Rest\View(
+     *     statusCode = 200
+     * )
      */
     public function listAction(UserRepository $userRepository)
     {
@@ -28,11 +33,58 @@ class UserController extends AbstractFOSRestController
     }
 
     /**
-     * @Get("/{id}", name="api_users_show_one", requirements= {"id"="\d+"})
-     * @View
+     * @Rest\Get("/{id}", name="api_users_show_one", requirements= {"id"="\d+"})
+     * @Rest\View(
+     *     statusCode = 200
+     * )
      */
     public function showAction(User $user)
     {
         return $user;
+    }
+
+    /**
+     * @Rest\Post(name="api_users_create_one")
+     * @Rest\View(
+     *     statusCode = 201
+     * )
+     * @ParamConverter(
+     *     "user",
+     *     converter="fos_rest.request_body"
+     * )
+     * @param User $user
+     * @param EntityManagerInterface $em
+     * @return void
+     */
+    public function createAction(
+        User $user,
+        EntityManagerInterface $em,
+        ClientRepository $clientRepository
+    ) {
+        $em->persist(
+            $user
+                ->setClient($clientRepository->findOneBy([]))
+                ->setCreatedAt(new \DateTimeImmutable())
+        );
+        $em->flush();
+
+        return $user;
+    }
+
+    /**
+     * @Rest\Delete("/{id}", name="api_users_delete_one")
+     * @Rest\View(
+     *     statusCode = 204
+     * )
+     * @param User $user
+     * @param EntityManagerInterface $em
+     * @return void
+     */
+    public function deleteAction(User $user, EntityManagerInterface $em)
+    {
+        $em->remove($user);
+        $em->flush();
+
+        return;
     }
 }
